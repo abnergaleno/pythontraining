80789962b61769bed17b140319fb1d37f3e44b6b
abnergaleno <abner.galeno@seer-technologies.com>
2016-04-28 18:26:41 +0800

    Update day2_exercise1.py

--- exercises/day2_exercise1.py
+++ exercises/day2_exercise1.py
@@ -4,12 +4,5 @@ def exerone(lwh=3):
 	threedimarr=[]
 	threedimarr = [[[str(x)+'-'+str(y)+'-'+str(z)  for z in range(3)] for y in range(3)] for x in range(3)]
 	print threedimarr
-	
-	for a_idx, a in enumerate(threedimarr):
-	for b_idx, b in enumerate(a):
-		for c_idx, c in enumerate(b):
-			# line += str(c)
-			print threedimarr[a_idx][b_idx][c_idx],
-	print '\n'
 
 exerone(3)
