18c329f135cd976ab486b7bb21d1b5a10567e4f0
abnergaleno <abner.galeno@seer-technologies.com>
2016-04-28 17:58:19 +0800

    Update day2_exercise3.py

--- exercises/day2_exercise3.py
+++ exercises/day2_exercise3.py
@@ -6,10 +6,6 @@ def exerthree(romaninput='XIV'):
 	decvals = [1000,500,100,50,10,5,1]
 	dec = 0
 	for l in romaninput:
-		print 'l = ' + l
-		print 'index = ' + str(romans.index(l))
-		print ' index2 = ' + str(min(romans.index(l)+1,len(romans)-1))
-
 		if decvals[romans.index(l)] < decvals[romans.index(romaninput[min(romaninput.index(l)+1,len(romaninput)-1)])]:
 			dec += decvals[romans.index(l)].__neg__() 
 			print '< ' + str(decvals[min(romans.index(l)+1,len(romans)-1)])
