85147a7b03385c4fb42f927be15e3e7578488c49
abnergaleno <abner.galeno@seer-technologies.com>
2016-04-28 19:55:21 +0800

    Update day2_exercise1.py

--- exercises/day2_exercise1.py
+++ exercises/day2_exercise1.py
@@ -2,7 +2,7 @@ def exerone(lwh=3):
 	print('exercise 1 : will create 3d array with value made up of 3d indices')
 
 	threedimarr=[]
-	threedimarr = [[[str(x)+'-'+str(y)+'-'+str(z)  for z in range(3)] for y in range(3)] for x in range(3)]
+	threedimarr = [[[str(x)+'-'+str(y)+'-'+str(z)  for z in range(lwh)] for y in range(lwh)] for x in range(lwh)]
 	print threedimarr
 
 exerone(3)
